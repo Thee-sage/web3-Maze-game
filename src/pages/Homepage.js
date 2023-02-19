dcf9f67a3d693210207d4b40a4326155c6f8d5f0
Abhijeet <anshhhh137@gmail.com>
2023-02-19 15:17:20 +0530

    new commit

---
+++ src/pages/Homepage.js
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useMoralisQuery, useMoralis } from "react-moralis";
+
+export default function Home() {
+  const { isWeb3Enabled } = useMoralis();
+  const { data: StakedNfts, isFetching: fetchingStakedNfts } = useMoralisQuery(
+    // TableName
+    // Function for the query
+    "ActiveItem",
+    (query) => query.limit(10).descending("tokenId")
+  );
+  console.log(StakedNfts);
+
+  return (
+    <div className="container mx-auto">
+      <h1 className="py-4 px-4 font-bold text-2xl">Recently staked</h1>
+      <div className="flex flex-wrap">
+        {isWeb3Enabled ? (
+          fetchingStakedNfts ? (
+            <div>Loading...</div>
+          ) : (
+            StakedNfts.map((nft) => {
+              console.log(nft.attributes);
+              const { price, nftAddress, tokenId, marketplaceAddress, seller } =
+                nft.attributes;
+              return (
+              );
+            })
+          )
+        ) : (
+          <div>Web3 Currently Not Enabled</div>
+        )}
+      </div>
+    </div>
+  );
+}
